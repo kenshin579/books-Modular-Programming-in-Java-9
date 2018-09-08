7bc3a84cd30cf44939644ca7c50db35712e54eca
ykoh <kenshin579@gmail.com>
2018-09-08 13:53:42 +0900

    reformat

--- 10-migrating-application/01-app-migration/src/com/packt/sortstrings/input/UserInputUtil.java
+++ 10-migrating-application/01-app-migration/src/com/packt/sortstrings/input/UserInputUtil.java
@@ -3,15 +3,14 @@ package com.packt.sortstrings.input;
 import java.util.Scanner;
 
 public class UserInputUtil {
-	Scanner scanner = new Scanner(System.in);
-	
-	public String getUserInput(String prompt) {
-		System.out.print(prompt);
-		return scanner.nextLine();
-	}
-	
-	public void close() {
-		scanner.close();
-	}
+    Scanner scanner = new Scanner(System.in);
 
+    public String getUserInput(String prompt) {
+        System.out.print(prompt);
+        return scanner.nextLine();
+    }
+
+    public void close() {
+        scanner.close();
+    }
 }
