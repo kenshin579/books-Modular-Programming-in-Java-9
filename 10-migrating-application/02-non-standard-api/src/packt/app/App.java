b7c03765378972bb7bd727776f8f772e477e6221
ykoh <kenshin579@gmail.com>
2018-09-08 14:20:22 +0900

    reformat

--- 10-migrating-application/02-non-standard-api/src/packt/app/App.java
+++ 10-migrating-application/02-non-standard-api/src/packt/app/App.java
@@ -6,9 +6,8 @@ import sun.util.calendar.CalendarUtils;
 
 public class App {
 
-	public static void main(String[] args) {
-		BASE64Encoder enc = new BASE64Encoder();
-		 CalendarUtils.isGregorianLeapYear(2018);
-	}
-
+    public static void main(String[] args) {
+        BASE64Encoder enc = new BASE64Encoder();
+        CalendarUtils.isGregorianLeapYear(2018);
+    }
 }
--- 10-migrating-application/02-non-standard-api/src/packt/app/AppFixed.java
+++ 10-migrating-application/02-non-standard-api/src/packt/app/AppFixed.java
@@ -3,13 +3,14 @@ package com.packt.app;
 
 import java.util.Base64;
 import java.util.Base64.Encoder;
+
 import sun.util.calendar.CalendarUtils;
 
 public class AppFixed {
 
-	public static void main(String[] args) {
-		Encoder enc = Base64.getEncoder();
-		CalendarUtils.isGregorianLeapYear(2018);
-	}
+    public static void main(String[] args) {
+        Encoder enc = Base64.getEncoder();
+        CalendarUtils.isGregorianLeapYear(2018);
+    }
 
 }
